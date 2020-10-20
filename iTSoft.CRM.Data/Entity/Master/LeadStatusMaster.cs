95a05b776e43df080476b870e92c76f2610aed22
DELL\Swati <swati.satpute3@gmail.com>
2020-10-20 20:37:16 +0530

    Dapper Updated

--- iTSoft.CRM.Data/Entity/Master/LeadStatusMaster.cs
+++ iTSoft.CRM.Data/Entity/Master/LeadStatusMaster.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using Dapper.Contrib.Extensions;
 namespace iTSoft.CRM.Data.Entity.Master
 {
     [Table("LeadStatusMaster")]
---
+++ iTSoft.CRM.Data/Entity/Process/FollowUpMaster.cs
@@ -0,0 +1,38 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iTSoft.CRM.Data.Entity.Process
+{
+    [Table("FollowupMaster")]
+    public class FollowupMaster
+    {
+        [Key]
+        public long FollowUpId { get; set; }
+
+        public long? RequestId { get; set; }
+
+        public DateTime? FollowUpDate { get; set; }
+
+        public long? LeadStatusId { get; set; }
+
+        public long? StageId { get; set; }
+
+        public string Remark { get; set; }
+
+        public string Attempt { get; set; }
+
+        public long? ClientRating { get; set; }
+
+        public long? AddedBy { get; set; }
+
+        public DateTime? AddedOn { get; set; }
+
+        public long? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+
+}
