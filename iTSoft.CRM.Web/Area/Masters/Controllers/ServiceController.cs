93c7c58f5549ec1dc936d5b5b54a34ed3cc8e4d4
DELL\Swati <swati.satpute3@gmail.com>
2020-10-31 22:23:04 +0530

    Service master save logic updated

--- iTSoft.CRM.Web/Area/Masters/Controllers/ServiceController.cs
+++ iTSoft.CRM.Web/Area/Masters/Controllers/ServiceController.cs
@@ -20,19 +20,41 @@ namespace iTSoft.CRM.Web.Area.Masters.Controllers
             ServiceResponse response = new ServiceResponse();
             try
             {
+
+                if(service == null)
+                {
+                    return BadRequest("Invalid input data");
+                }
+
                 if (service.ServiceId == 0)
                 {
-                    response.ResponseCode = base.genericService.Add(service) > 0 ? ResponseCode.Success : ResponseCode.DataBaseError;
+
+                    if (base.genericService.GetAll().Where(c => c.ServiceName == service.ServiceName).Count() == 0)
+                    {
+                        response.ResponseCode = base.genericService.Add(service) > 0 ? ResponseCode.Success : ResponseCode.DataBaseError;
+                    }
+                    else
+                    {
+                        return BadRequest("Service with same name already exists");
+                    }
                 }
                 else
                 {
-                    response.ResponseCode = base.genericService.Update(service)  == true ? ResponseCode.Success : ResponseCode.DataBaseError;
+                    if (base.genericService.GetAll().Where(c => c.ServiceName == service.ServiceName && c.ServiceId != service.ServiceId).Count() == 0)
+                    {
+                        response.ResponseCode = base.genericService.Update(service) == true ? ResponseCode.Success : ResponseCode.DataBaseError;
+                    }
+                    else
+                    {
+                        return BadRequest("Service with same name already exists");
+                    }
                 }
             }
             catch (Exception ex)
             {
-                response.ResponseCode = ResponseCode.ApplicationError;
                 base._logger.Error(ex, "ServiceController - Save");
+                return BadRequest("Internal service error");
+
             }
             return Ok(response);
         }
